f9d639083f592cc82472bfa3b86d9593b0432098
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2022-12-27 19:05:46 +0100

    Esercizio Completato

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+// Chiediamo i 5 numeri
+
+// Primo
+const firstNum = parseInt(prompt('Scrivi il primo numero.'));
+
+console.log('Primo:', firstNum);
+
+// Secondo
+const secondNum = parseInt(prompt('Scrivi il secondo numero.'));
+
+console.log('Secondo:', secondNum);
+
+// Terzo
+const thirdNum = parseInt(prompt('Scrivi il terzo numero.'));
+
+console.log('Terzo:', thirdNum);
+
+// Quarto
+const fourthNum = parseInt(prompt('Scrivi il quarto numero.'));
+
+console.log('Quarto:', fourthNum);
+
+// Quinto
+const fifthNum = parseInt(prompt('Scrivi il quinto numero.'));
+
+console.log('Quinto:', fifthNum);
+
+// Calcoliamo la somma 
+
+let somma = firstNum + secondNum + thirdNum + fourthNum + fifthNum;
+
+console.log('La somma è:', somma);
+
+// Calcoliamo la media 
+
+let media = ((firstNum + secondNum + thirdNum + fourthNum + fifthNum) / 5);
+
+console.log('La media è:', media);
+
+
+
+
